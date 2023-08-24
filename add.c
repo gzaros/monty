d6f91f7104730b6c3825efa49850223c9fe98932
gzaros <templatewebvo@gmail.com>
2023-08-24 23:32:53 +0100

    monty

--- add.c
+++ add.c
@@ -1,27 +1,25 @@
 #include <stdio.h>
+#include "monty.h"
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include "monty.h"
 
 /**
- * _add -  adds the first two nodes of the stack
- * @stack: stack given by main
- * @line_cnt: line counter
+ * @stack: stack given by hand
+ * @row_cnt: Row counter
+ * e_add -  add the first two nodes of the stack
  *
  * Return: void
  */
-void _add(stack_t **stack, unsigned int line_cnt)
+void _add(stack_t **stack, unsigned int row_cnt)
 {
-	int result;
-
+	int val;
 	if (!stack || !*stack || !((*stack)->next))
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
+		fprintf(stderr, "L%d: can't add, stack too short\n", row_cnt);
 		exit(EXIT_FAILURE);
 	}
-
-	result = ((*stack)->next->n) + ((*stack)->n);
-	pop(stack, line_cnt); /*For top node*/
-	(*stack)->n = result;
+	val = ((*stack)->next->n) + ((*stack)->n);
+	pop(stack, row_cnt); /*For the top node*/
+	(*stack)->n = val;
 }
