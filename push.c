584bfcd5d20698d0ae7130ca484b83d04b5775e7
gzaros <templatewebvo@gmail.com>
2023-08-25 01:11:52 +0100

    monty

--- push.c
+++ push.c
@@ -1,38 +1,49 @@
 #include <stdio.h>
+#include "monty.h"
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include "monty.h"
 
 /**
- * push - push element into the stack
+ * push - Add the element to the stack.
  * @stack: stack given by main
- * @line_cnt: amount of lines
+ * @row_cnt: Row counter
  *
  * Return: void
  */
-void push(stack_t **stack, unsigned int line_cnt)
-{
-	char *n = global.argument;
 
-	if ((is_digit(n)) == 0)
+
+void push(stack_t **stack, unsigned int row_cnt)
+{
+	char *number = global.argument;
+
+	if ((is_digit(number)) == 0)
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+
+		fprintf(stderr, "L%d: usage: push integer\n", row_cnt);
+
 		exit(EXIT_FAILURE);
+
 	}
+
 
 	if (global.data_struct == 1)
 	{
+
 		if (!add_node(stack, atoi(global.argument)))
 		{
+
 			exit(EXIT_FAILURE);
+
 		}
 	}
 	else
 	{
 		if (!queue_node(stack, atoi(global.argument)))
 		{
+
 			exit(EXIT_FAILURE);
+
 		}
 	}
 }
