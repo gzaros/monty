61b7b4d212b9c5886e779e7a8b25a2370d6c3061
gzaros <templatewebvo@gmail.com>
2023-08-25 01:10:13 +0100

    monty

--- pint.c
+++ pint.c
@@ -4,19 +4,24 @@
 #include "monty.h"
 
 /**
- * pint - print the value at the top of the stack
- * @stack: stack given by main in start.c
- * @line_cnt: amount of lines
+ * pint - Output the value at the top of the stack.
+ * @stack: stack given by hand in start.c
+ * @row_cnt: Row counter
  *
  * Return: void
  */
-void pint(stack_t **stack, unsigned int line_cnt)
+
+void pint(stack_t **stack, unsigned int row_cnt)
 {
-	if (!stack || !(*stack))
+
+	if (!(*stack) || !stack)
 	{
-		fprintf(stderr, "L%d: can't pint, stack empty\n", line_cnt);
+
+		fprintf(stderr, "L%d: can't pint, stack empty\n", row_cnt);
+
 		exit(EXIT_FAILURE);
 
 	}
+
 	printf("%d\n", (*stack)->n);
 }
