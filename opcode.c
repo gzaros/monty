52b66d868e7aa9825c505435eb31917a623f1ed2
gzaros <templatewebvo@gmail.com>
2023-08-25 01:06:45 +0100

    monty

--- opcode.c
+++ opcode.c
@@ -7,36 +7,40 @@
  * opcode - function in charge of running builtins
  * @stack: stack given by main
  * @str: string to compare
- * @line_cnt: amount of lines
+ * @row_cnt: row counter
  *
  * Return: nothing
  */
-void opcode(stack_t **stack, char *str, unsigned int line_cnt)
+void opcode(stack_t **stack, char *str, unsigned int row_cnt)
 {
-	int i = 0;
 
-	instruction_t op[] = INSTRUCTIONS;
+
+	instruction_t op[] = INSTRUCTIONS;	
+	
+	int ii = 0;
 
 	if (!strcmp(str, "stack"))
 	{
 		global.data_struct = 1;
 		return;
 	}
+
 	if (!strcmp(str, "queue"))
 	{
 		global.data_struct = 0;
 		return;
 	}
 
-	while (op[i].opcode)
+	while (op[ii].opcode)
 	{
-		if (strcmp(op[i].opcode, str) == 0)
+		if (strcmp(op[ii].opcode, str) == 0)
 		{
-			op[i].f(stack, line_cnt);
-			return; /* if we found a match, run the function */
+			op[ii].f(stack, row_cnt);
+			return; 
 		}
 		i++;
 	}
-	fprintf(stderr, "L%d: unknown instruction %s\n", line_cnt, str);
+	
+	fprintf(stderr, "L%d: unknown instruction %s\n", row_cnt, str);
 	exit(EXIT_FAILURE);
 }
