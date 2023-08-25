c014dbcb9a2cd7a595b4a9746ae5e46989362f33
gzaros <templatewebvo@gmail.com>
2023-08-25 01:04:27 +0100

    monty

--- nop.c
+++ nop.c
@@ -1,14 +1,14 @@
 #include "monty.h"
 
 /**
- * _nop - do nothing
+ * nop - do nothing
  * @row_cnt : nothing
  * @stack: it does not matter
  *
  * Return: do (nothing)
  */
 
-void _nop(stack_t **stack, unsigned int row_cnt)
+void nop(stack_t **stack, unsigned int row_cnt)
 {
 
 	(void) row_cnt;
